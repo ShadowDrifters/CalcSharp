db57c0ea4ebc299ddd6b8834a8619efd83afc625
Василий Василий <xvas@mail.ru>
2020-10-03 19:17:53 +0700

    Прописал кнопки 0-9

--- CalcSharp/Form1.Designer.cs
+++ CalcSharp/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.DisplayText = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -40,16 +40,17 @@
             this.button9 = new System.Windows.Forms.Button();
             this.button10 = new System.Windows.Forms.Button();
             this.button11 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // DisplayText
             // 
-            this.textBox1.Location = new System.Drawing.Point(47, 25);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(318, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TabStop = false;
+            this.DisplayText.Location = new System.Drawing.Point(47, 25);
+            this.DisplayText.Name = "DisplayText";
+            this.DisplayText.ReadOnly = true;
+            this.DisplayText.Size = new System.Drawing.Size(318, 20);
+            this.DisplayText.TabIndex = 0;
+            this.DisplayText.TabStop = false;
             // 
             // button1
             // 
@@ -69,6 +70,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -78,6 +80,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -87,6 +90,7 @@
             this.button4.TabIndex = 4;
             this.button4.Text = "4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -96,6 +100,7 @@
             this.button5.TabIndex = 5;
             this.button5.Text = "5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -105,6 +110,7 @@
             this.button6.TabIndex = 6;
             this.button6.Text = "6";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -114,6 +120,7 @@
             this.button7.TabIndex = 7;
             this.button7.Text = "7";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -123,6 +130,7 @@
             this.button8.TabIndex = 8;
             this.button8.Text = "8";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -132,6 +140,7 @@
             this.button9.TabIndex = 9;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -151,6 +160,16 @@
             this.button11.Text = "+";
             this.button11.UseVisualStyleBackColor = true;
             // 
+            // button0
+            // 
+            this.button0.Location = new System.Drawing.Point(119, 332);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(45, 33);
+            this.button0.TabIndex = 9;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -158,6 +177,7 @@
             this.ClientSize = new System.Drawing.Size(413, 377);
             this.Controls.Add(this.button11);
             this.Controls.Add(this.button10);
+            this.Controls.Add(this.button0);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -167,7 +187,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DisplayText);
             this.Name = "Form1";
             this.Text = "Канкулятор";
             this.ResumeLayout(false);
@@ -177,7 +197,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox DisplayText;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
@@ -189,6 +209,7 @@
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button10;
         private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button0;
     }
 }
 
--- CalcSharp/Form1.cs
+++ CalcSharp/Form1.cs
@@ -19,7 +19,82 @@ namespace CalcSharp
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "1";
+            else
+                DisplayText.AppendText("1");
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "2";
+            else
+                DisplayText.AppendText("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "3";
+            else
+                DisplayText.AppendText("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "4";
+            else
+                DisplayText.AppendText("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "5";
+            else
+                DisplayText.AppendText("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "6";
+            else
+                DisplayText.AppendText("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "7";
+            else
+                DisplayText.AppendText("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "8";
+            else
+                DisplayText.AppendText("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "9";
+            else
+                DisplayText.AppendText("9");
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (DisplayText.Text == "0")
+                DisplayText.Text = "0";
+            else
+                DisplayText.AppendText("0");
         }
     }
 }
