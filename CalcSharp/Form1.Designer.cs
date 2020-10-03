5f4afeb8b2fe39e5794f3eb7c752a09a893ce8aa
Василий Василий <xvas@mail.ru>
2020-10-03 18:38:17 +0700

    Добавил кн. "С"

--- CalcSharp/Form1.Designer.cs
+++ CalcSharp/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -129,11 +130,21 @@
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
             // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(304, 292);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(50, 33);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "С";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(413, 377);
+            this.Controls.Add(this.button10);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -163,6 +174,7 @@
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
 
